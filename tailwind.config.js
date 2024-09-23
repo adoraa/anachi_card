5a2b4ab283a258e3c7fb1b7156b75b69a93aa28c
Adora <adoraonyile@gmail.com>
2024-09-23 19:40:25 +0100

    bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'clouds': 'url("images/svg/endless-clouds.svg")'
+      },
+      fontFamily: {
+        'play': ['Playwrite DE Grund']
+      }
+    },
   },
   plugins: [],
 }
